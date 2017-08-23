cbab694564ba0fff855c2524ee3f386ced333268
Marnie Auld <MarnieLee.Auld@my.jcu.edu.au>
2017-08-23 21:10:31 +1000

    add function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -12,18 +12,18 @@ def main():
     for month in range(1, number_months + 1):
         income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
-    print_report(incomes)
+
+    print_income_report(incomes)
 
 
-def print_report(incomes):
+def print_income_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
     number_months = len(incomes)
     for month in range(1, number_months + 1):
         income = incomes[month - 1]
         total += income
-
-    print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
 main()

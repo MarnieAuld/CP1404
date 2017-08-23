25796ac832e23ae933663e1509ebf81471581500
Marnie Auld <MarnieLee.Auld@my.jcu.edu.au>
2017-08-23 21:26:43 +1000

    add random generator

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -1,4 +1,5 @@
-""" import random
+"""
+import random
 user input for number of quick picks, while loop for error checking < 0
 print number of quick picks in blocks of 6 NUMBERS, MINIMUM_NUMBER = 1, MAXIMUM_NUMBER = 45
 """
@@ -13,22 +14,10 @@ def main():
         print("Invalid Input")
         number_of_tickets = int(input("How many Quick Picks? "))
 
-    number = random.randint(1, 45)
-    print(number)
-
+    for number in range(number_of_tickets):
+        for i in range(6):
+            number = random.randint(1, 45)
+            print("{:3}".format(number), end=' ')
+        print()
 
 main()
-
-
-
-
-
-
-
-
-
-for i in range(quick_picks):
-    for j in range(NUMBERS_PER_LINE):
-        number = random.randint(MINIMUM, MAXIMUM)
-        print("{:2}".format(number), end=' ')
-    print()

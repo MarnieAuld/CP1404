c2d00cea2edb0bb6ae531822817df7377c480959
Marnie Auld <MarnieLee.Auld@my.jcu.edu.au>
2017-08-23 12:07:00 +1000

    add files

---
+++ prac_04/lottery_ticket_generator.py
@@ -0,0 +1,34 @@
+""" import random
+user input for number of quick picks, while loop for error checking < 0
+print number of quick picks in blocks of 6 NUMBERS, MINIMUM_NUMBER = 1, MAXIMUM_NUMBER = 45
+"""
+
+import random
+
+
+def main():
+
+    number_of_tickets = int(input("How many Quick Picks? "))
+    while number_of_tickets < 0:
+        print("Invalid Input")
+        number_of_tickets = int(input("How many Quick Picks? "))
+
+    number = random.randint(1, 45)
+    print(number)
+
+
+main()
+
+
+
+
+
+
+
+
+
+for i in range(quick_picks):
+    for j in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM, MAXIMUM)
+        print("{:2}".format(number), end=' ')
+    print()
---
+++ prac_04/number_list.py
@@ -0,0 +1,10 @@
+numbers = []
+for i in range(5):
+    user_number = int(input("Enter a number:"))
+    numbers.append(user_number)
+
+print("The first number is ", numbers[0])
+print("The last number is ", numbers[-1])
+print("The smallest number is ", )
+print("The largest number is ", )
+print("The aver of the numbers is ", )

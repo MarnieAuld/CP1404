02dcea6aec4e815ed4aeaa2a6d9cd0217e25859b
Marnie Auld <MarnieLee.Auld@my.jcu.edu.au>
2017-08-29 22:17:08 +1000

    complete exercise

---
+++ prac_05/hexadecimal_colour_codes.py
@@ -0,0 +1,15 @@
+"""
+Hexadecimal colours in a dictionary
+"""
+
+COLOUR_NAMES = {"MINTGREEN": "#f5fffa", "VIOLET": "#ee82ee", "STEELBLUE": "#4682b4", "SANDYBROWN": "#f4a460",
+                "PALEGREEN": "#98fb98", "ORCHID": "#da70d6", "NAVYBLUE": "#000080", "MAGENTA": "#ff00ff",
+                "LIMEGREEN": "#32cd32", "HOTPINK": "#ff69b4", "FIREBRICK": "#b22222"}
+
+user_colour = str(input("Enter colour name: ").upper().replace(" ", ""))
+while user_colour != "":
+    if user_colour in COLOUR_NAMES:
+        print(user_colour, "hexadecimal code: ", COLOUR_NAMES[user_colour])
+    else:
+        print("Invalid colour name")
+    user_colour = str(input("Enter colour name: ").upper().replace(" ", ""))

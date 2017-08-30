35e43ae7c1b69891002ccf8b0b8956161624d503
jc300916 <mummy23#>
2017-08-30 13:26:50 +1000

    add string formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,13 +5,18 @@ File needs reformatting
 """
 
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
 state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
+        print("{} is {}".format(state, STATE_NAMES[state]))
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
+
+"""
+string formatting state names in a loop
+"""
+for state_name in STATE_NAMES:
+    print("{:3} is {}".format(state_name, STATE_NAMES[state_name]))

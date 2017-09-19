ef9453e9802f56112f659542e6e5b099c7a0d9ba
marnieauld <marnieauld@gmail.com>
2017-09-20 09:32:13 +1000

    add init function

---
+++ prac_07/programming_language.py
@@ -0,0 +1,9 @@
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise ProgrammingLanguage from given field types"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year

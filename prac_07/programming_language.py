046d0560035245250957a40a4c493bc5c48fd345
marnieauld <marnieauld@gmail.com>
2017-09-20 09:40:51 +1000

    add is_dynamic() function

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,6 +1,10 @@
+"""
+Intermediate Exercise - Programming Language
+Store multiple field values describing various program languages.
+"""
+
 
 class ProgrammingLanguage:
-
     def __init__(self, name, typing, reflection, year):
         """Initialise ProgrammingLanguage from given field types"""
         self.name = name
@@ -12,3 +16,7 @@ class ProgrammingLanguage:
         """Return string"""
         return "{}, {} Typing, Reflection={}, First appeared in {}".\
             format(self.name, self.typing, self.reflection, self.year)
+
+    def is_dynamic(self):
+        """Determines wether language is dynamically typed - returns TRUE/FALSE """
+        return self.typing == "Dynamic"

819c25f1dd127c538dc54382b7dc5cac67d123ce
marnieauld <marnieauld@gmail.com>
2017-09-20 14:49:14 +1000

    remove constants from global and add TODO

--- prac_07/guitars.py
+++ prac_07/guitars.py
@@ -9,22 +9,21 @@ Guitar 3:       Line 6 JTV-59 (2010),  worth $  1,512.90
 """
 from prac_07.guitar import Guitar
 
-guitars = []
-guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
-
 
 def main():
     """Print output to match sample output"""
+    guitars = [Guitar("Fender Stratocaster", 2014, 765.40), Guitar("Gibson L-5 CES", 1922, 16035.40),
+               Guitar("Line 6 JTV-59", 2010, 1512.90)]
     for i, guitar in enumerate(guitars):
+        # TODO: replace next 4 lines with ternary operator
         if guitar.is_vintage():
             vintage_string = "(vintage)"
         else:
             vintage_string = ""
         print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".
-              format(i+1, guitar.name, guitar.year, guitar.cost, vintage_string))
+              format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
         # print("Guitar {}: {self.name} ({self.year}), worth ${self.cost}".format(i+1, self = guitar))
         # Can you use placeholders if you have format variables outside self - and how do you string format
+
 
 main()

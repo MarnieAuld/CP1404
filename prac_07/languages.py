27bbdf9a2061ddb74787bab15601999422996b35
marnieauld <marnieauld@gmail.com>
2017-09-20 12:55:56 +1000

    print blank line to differentiate exercise parts

--- prac_07/languages.py
+++ prac_07/languages.py
@@ -9,11 +9,12 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
     print(ruby)
     print(python)
     print(visual_basic)
+    print()
     languages = [ruby, python, visual_basic]
-
     print("The dynamically typed languages are:")
     for language in languages:
         if language.is_dynamic():

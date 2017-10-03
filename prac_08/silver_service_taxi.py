973fdcb3721b4df626d9a5de0b6e3ea55c83be59
marnieauld <marnieauld@gmail.com>
2017-10-03 19:21:02 +1000

    add init and str function for SilverServiceTaxi class

---
+++ prac_08/silver_service_taxi.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Practical 8 - SilverServiceTaxi
+Specialised taxi class, derived from Taxi class
+"""
+
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of Taxi"""
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string specifically for SilverServiceTaxi."""
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(),
+                                                    self.flagfall)

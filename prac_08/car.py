6e9bc2dccf3202787754f3450847f53235149b7f
marnieauld <marnieauld@gmail.com>
2017-10-03 16:59:39 +1000

    add file

---
+++ prac_08/car.py
@@ -0,0 +1,34 @@
+"""CP1404/CP5632 Practical - Car class example."""
+
+
+class Car:
+    """Car object."""
+    def __init__(self, name="", fuel=0):
+        """Initialise a car instance.
+        name: string name for car
+        fuel: integer value
+        """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+
+    def __str__(self):
+        """Return a string"""
+        return "{}, fuel = {}, odometer = {}".format(self.name, self.fuel, self.odometer)
+
+    def add_fuel(self, amount):
+        """Add amount to the fuel total."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a specified distance.
+        If car has enough fuel, drive distance
+        Else drive until fuel = 0
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
---
+++ prac_08/taxi_test.py
@@ -0,0 +1,9 @@
+"""
+CP1404 Practical 8 - TAXI
+1. Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+2. Drive the taxi 40km
+3. Print the taxi's details and the current fare
+4. Restart the meter (start a new fare) and then drive the car 100km
+5. Print the details and the current fare
+"""
+

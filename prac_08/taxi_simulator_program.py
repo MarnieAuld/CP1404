4c6bd25cdfc7462e3c376df3262ab53295bb0b27
marnieauld <marnieauld@gmail.com>
2017-10-03 20:05:29 +1000

    add docstring

---
+++ prac_08/taxi_simulator_program.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical 8 - Taxi Simulator Program
+"""
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+from prac_08.taxi import Taxi
+from prac_08.car import Car
+
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer, 200, 4")]
+
+MENU = """q)uit,
+c)hoose taxi,
+d)rive
+"""
+
+
+def main():
+    pass
+
+main()

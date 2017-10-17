91f378108a17d1c9d277e5c5080c2332d49ac7c1
marnieauld <marnieauld@gmail.com>
2017-10-18 01:26:46 +1000

    work done on file - renaming works

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -11,12 +11,7 @@ def main():
     print("Current directory is", os.getcwd())
 
     # change to desired directory
-    os.chdir('Lyrics/Christmas')
-    # print a list of all files (test)
-    print(os.listdir('.'))
-
-    # make a new directory
-    # os.mkdir('temp')
+    os.chdir('Lyrics')
 
     # loop through each file in the (original) directory
     for filename in os.listdir('.'):
@@ -24,21 +19,20 @@ def main():
         if not os.path.isdir(filename):
             new_name = get_fixed_filename(filename)
             print(new_name)
-
-            # Option 1: rename file to new name - in place
-            # os.rename(filename, new_name)
-
-            # Option 2: move file to new place, with new name
-            # shutil.move(filename, 'temp/' + new_name)
-
-            # Processing subdirectories using os.walk()
-
-            # os.chdir('..')  # .. means "up" one directory
-            # for dir_name, subdir_list, file_list in os.walk('.'):
-            #     print("In", dir_name)
-            #     print("\tcontains subdirectories:", subdir_list)
-            #     print("\tand files:", file_list)
-
+    #
+    #         # Option 1: rename file to new name - in place
+            os.rename(filename, new_name)
+    #
+    #         # Option 2: move file to new place, with new name
+    #         # shutil.move(filename, 'temp/' + new_name)
+    #
+    #         # Processing subdirectories using os.walk()
+    #
+    #         # os.chdir('..')  # .. means "up" one directory
+    for dir_name, subdir_list, file_list in os.walk('.'):
+        print("In", dir_name)
+        print("\tcontains subdirectories:", subdir_list)
+        print("\tand files:", file_list)
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename.
@@ -48,14 +42,17 @@ def get_fixed_filename(filename):
     """
     filename = filename.replace(" ", "_").replace(".TXT", ".txt")
     if "_" not in filename:
-        for index, character in enumerate(filename):
-            if character.isupper() and filename[index-1].islower():
-                pass
+        characters = list(filename)
+        for index, character in enumerate(characters):
+            if character.isupper() and characters[index-1].isalpha() and index > 0:
+                characters.insert(index, "_")
+        filename = "".join(characters)
     else:
-        for index, character in enumerate(filename):
+        characters = list(filename)
+        for index, character in enumerate(characters):
             if character == "_":
-                filename(index+1)
-
+                characters[index+1] = characters[index+1].upper()
+        filename = "".join(characters)
     new_name = filename
     return new_name
 

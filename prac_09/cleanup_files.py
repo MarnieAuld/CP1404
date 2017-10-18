e95386b93cfabdd2a85ac83d58e008ea06812369
marnieauld <marnieauld@gmail.com>
2017-10-18 13:42:33 +1000

    remove blank lines

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -13,24 +13,20 @@ def main():
     # change to desired directory
     os.chdir('Lyrics')
 
-    # make a new directory
-    # os.mkdir('temp')
-
     # loop through each file in the (original) directory
     # for filename in os.listdir('.'):
     #     # ignore directories, just process files
     #     if not os.path.isdir(filename):
     #         new_name = get_fixed_filename(filename)
 
-            # Option 1: rename file to new name - in place
-            # os.rename(filename, new_name)
+    # Option 1: rename file to new name - in place
+    # os.rename(filename, new_name)
 
-            # Option 2: move file to new place, with new name
-            # shutil.move(filename, 'temp/' + new_name)
+    # Option 2: move file to new place, with new name
+    # shutil.move(filename, 'temp/' + new_name)
 
-            # Processing subdirectories using os.walk()
-
-            # os.chdir('..')  # .. means "up" one directory
+    # Processing subdirectories using os.walk()
+    # os.chdir('..')  # .. means "up" one directory
 
     for dir_name, subdir_list, file_list in os.walk('.'):
         print("In", dir_name)

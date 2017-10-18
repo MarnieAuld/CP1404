b9c4285bba26772d5147faef641350d7a86a656a
marnieauld <marnieauld@gmail.com>
2017-10-18 14:52:47 +1000

    remove enumerate and 'is in' statement

--- prac_09/file_sorting_V1.py
+++ prac_09/file_sorting_V1.py
@@ -14,14 +14,13 @@ def main():
     # change to desired directory
     os.chdir('FilesToSort')
 
-    file_types = ["docx", "doc", "png", "gif", "txt", "xlsx", "xls", "jpg"]     # this only works if docx runs before doc, and xlsx runs before xls
-
-    for index, file_type in enumerate(file_types):
-        type_name = file_types[index]
+    file_types = ["docx", "doc", "png", "gif", "txt", "xlsx", "xls", "jpg"]
+    
+    for type_name in file_types:
         os.mkdir(type_name)
         for filename in os.listdir('.'):
             if not os.path.isdir(filename):
-                if type_name in filename:
+                if type_name == filename:
                     shutil.move(filename, type_name)
 
 

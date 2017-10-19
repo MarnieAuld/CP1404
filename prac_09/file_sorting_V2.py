fc16cebc0d0416600b2d43fc6c4597a3950540b0
marnieauld <marnieauld@gmail.com>
2017-10-19 12:33:00 +1000

    add extension finding

--- prac_09/file_sorting_V2.py
+++ prac_09/file_sorting_V2.py
@@ -14,15 +14,26 @@ def main():
     # change to desired directory
     os.chdir('FilesToSort(1)')
 
-    file_types = ["docx", "doc", "png", "gif", "txt", "xlsx", "xls", "jpg"]
+    # file_types = ["docx", "doc", "png", "gif", "txt", "xlsx", "xls", "jpg"]
+    file_types = []
+    for filename in os.listdir('.'):
+        file_extension = os.path.splitext(filename)[1]
+        if file_extension not in file_types:
+            file_types.append(file_extension)
 
     # make new directories
-    os.mkdir('Spreadsheets')
-    os.mkdir('Images')
-    os.mkdir('Docs')
+    # os.mkdir('Spreadsheets')
+    # os.mkdir('Images')
+    # os.mkdir('Docs')
 
     for file_type in file_types:
         chosen_folder = input("What category would you like to sort {} files into? ".format(file_type))
+        if not os.path.exists(chosen_folder):
+            os.mkdir(chosen_folder)
         for filename in os.listdir('.'):
             if not os.path.isdir(filename):
-                pass
+                if file_type in filename:
+                    shutil.move(filename, chosen_folder)
+
+
+main()

c2946be54e9edfd436e54a70a837e0e784cfff31
marnieauld <marnieauld@gmail.com>
2017-10-25 11:09:18 +1000

    complete phrase_to_sentence function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from prac_07.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -19,7 +19,7 @@ def is_long_word(word, length=5):
     False
     >>> is_long_word("supercalifrag")
     True
-    >>> is_long_word("Python", 6)
+    >>> is_long_word("Python", 5)
     True
     """
     return len(word) > length
@@ -32,35 +32,37 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # Write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car does not have correct fuel amount"
+    assert not test_car.fuel == 23
+    test_car = Car()
+    assert test_car.fuel == 0
+    assert not test_car.fuel == 10
+
+
+def phrase_to_sentence(phrase):
+    """ Reformat a given phrase and reutrn as a sentence (starting with capital and ending with full stop).
+        >>> phrase_to_sentence('hello')
+        'Hello.'
+        >>> phrase_to_sentence('It is an ex parrot.')
+        'It is an ex parrot.'
+        >>> phrase_to_sentence('change me into a sentence, please')
+        'Change me into a sentence, please.'
+        """
+    if phrase[-1] != ".":
+        phrase += "."
+    return phrase.capitalize()
 
 
 run_tests()
-
-# TODO: 3. Uncomment the following line and run the doctests
-# doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (don't change the tests, but the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests so that:
-# 'hello' -> ''Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (that's valid!)
-# then write the body of the function so that the tests pass
+doctest.testmod()

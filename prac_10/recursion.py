db8c6ffcb44afc33f0702a4981f5d2cd54def89e
marnieauld <marnieauld@gmail.com>
2017-10-25 14:31:06 +1000

    move all call to functions to bottom of program

--- prac_10/recursion.py
+++ prac_10/recursion.py
@@ -11,9 +11,6 @@ def do_it(n):
     return n % 2 + do_it(n - 1)
 
 
-# print(do_it(5))
-
-
 def do_something(n):
     """Print the squares of positive numbers from n down to 0."""
     if n < 0:
@@ -22,14 +19,13 @@ def do_something(n):
     do_something(n - 1)
 
 
-# do_something(4)
-
-
 def do_opposite_of_something(n):
     """Print the squares of positive numbers from 0 to n or their way back after hitting base case"""
     if n >= 0:
         do_opposite_of_something(n - 1)
         print(n ** 2)
-    # TODO: this isn't right I dont think
 
+
+# print(do_it(5))
+# do_something(4)
 # do_opposite_of_something(4)
